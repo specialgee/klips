eb2f6638c4fbdd3698829d3990fda883d13ef6a2
gee <special.geeee@gmail.com>
2019-10-31 18:55:42 +0100

    added vx shell script

---
+++ vx.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+# transform a video file with 16:9 aspect ratio into 4:3
+# USAGE: vx input output
+function vx() {
+    help() {
+      echo "usage: vx [input] [output]"
+      echo "-input        file input path"
+      echo "-output       file output path"
+      echo ""
+    }
+    if [[ "$#" -eq 0 ]]; then
+      help
+    else
+      help
+      ffmpeg -i $1 -filter:v "pad=iw:iw*3/4:(ow-iw)/2:(oh-ih)/2" -c:a copy $2
+    fi
+}
