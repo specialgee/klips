b02f7bc892cb4ab7811eecb1293af6a75b01ada8
gee <special.geeee@gmail.com>
2019-11-25 22:29:19 +0100

    added youtube to mp3 conversion with optional format parameter
    download mp4 by default

--- yt.sh
+++ yt.sh
@@ -1,16 +1,31 @@
 #!/usr/bin/bash
-# download video from Youtube with youtube-dl
-# USAGE: yt URL
+# download audio/video from Youtube with youtube-dl
+# USAGE: yt [format] url
 function yt() {
     help() {
-      echo "usage: yt [url]"
+      echo "usage: yt [format] url"
       echo "-url          youtube video url"
+      echo "-format       currently supported: mp3, mp4"
       echo ""
     }
     if [[ "$#" -eq 0 ]]; then
       help
+    elif [[ "$#" -eq 1 ]]; then
+      help
+
+      youtube-dl --recode-video "mp4" -o "/c/Users/gllm/Desktop/%(title)s.%(ext)s" $1
     else
       help
-      youtube-dl --recode-video "mp4" -o "/c/Users/gllm/Desktop/%(title)s.%(ext)s" $1
+
+      format=$1
+ 
+      if [[ $format == "mp3" ]]; then
+          youtube-dl -x --audio-format "mp3" -o "/c/Users/gllm/Desktop/%(title)s.%(ext)s" $2
+      elif [[ $format == "mp4" ]]; then
+          youtube-dl --recode-video "mp4" -o "/c/Users/gllm/Desktop/%(title)s.%(ext)s" $2
+      else
+          echo
+      fi
+
     fi
 }
